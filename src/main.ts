1cd40ef06b081f08d9c1b6dd2a798c1238a161f6
jdfiallos <juandanielfiallos@gmail.com>
2025-01-18 21:59:38 -0600

    Add environment configuration and update dependencies

--- src/main.ts
+++ src/main.ts
@@ -14,10 +14,11 @@ async function bootstrap() {
       transform: true,
       transformOptions: {
         enableImplicitConversion: true,
-      }
-    })
+      },
+    }),
   );
 
-  await app.listen(3000);
+  console.log('Server is running on port', process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
